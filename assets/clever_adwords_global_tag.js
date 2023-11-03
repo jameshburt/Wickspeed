427fa778b539433c934d2e3e8ef98414facab6a5
jameshburt <78088871+jameshburt@users.noreply.github.com>
2023-11-02 23:24:45 -0400

    Add files via upload

---
+++ assets/clever_adwords_global_tag.js
@@ -0,0 +1,11 @@
+$.getScript('https://www.googletagmanager.com/gtag/js?id=AW-11392277673', function(){
+
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11392277673');
+
+
+});
